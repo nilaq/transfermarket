6d067c8642fd934297c4f5e6487647aeddd65ef7
nilaq <43445261+nilsbitzer@users.noreply.github.com>
2023-01-31 18:51:08 -0800

    update

--- data_cleaning.R
+++ data_cleaning.R
@@ -26,7 +26,8 @@ player_valuations_clean <- player_valuations %>%
   arrange(player_id, year, abs(as.Date(paste0(year, "-06-30")) - date)) %>%
   slice_head(n = 1L) %>%
   ungroup() %>%
-  select(-datetime, -dateweek, -current_club_id, -player_club_domestic_competition_id)
+  select(-datetime, -dateweek, -current_club_id, -player_club_domestic_competition_id) %>%
+  rename(date_valuation = date)
 
 # clean players
 players_clean <- players %>%
@@ -41,11 +42,20 @@ player_performance_metadata <- player_performance %>%
   mutate(name = stri_trans_general(name, "Latin-ASCII")) %>%
   group_by(name, birthyear, Year, age, nation) %>%
   summarise(position = last(position), team = last(team), league = last(league), n = n()) %>%
-  mutate(transfer = n > 1) %>%
+  mutate(transfer_during_season = n > 1) %>%
   select(-n) %>%
   ungroup() %>%
   left_join(players_clean %>% select(name, birthyear, player_id), by = c('name', 'birthyear')) %>%
-  filter(!is.na(player_id))
+  filter(!is.na(player_id)) %>%
+  group_by(player_id) %>%
+  mutate(transfer_between_season = ifelse(!is.na(lag(team)) & team != lag(team), T, F)) %>%
+  mutate(transfer = transfer_between_season | transfer_during_season) %>%
+  select(-transfer_between_season, -transfer_during_season) %>%
+  ungroup() %>%
+  left_join(league_performance %>% select(Year, Team, 'League Rank'), by = c('Year', 'team' = 'Team')) %>%
+  mutate(plays_for_top_team = !is.na(.$'League Rank') & .$'League Rank' <= 5) %>%
+  select(-'League Rank')
+
 
 player_performance_raw <- player_performance %>%
   mutate(name = stri_trans_general(name, "Latin-ASCII")) %>%
@@ -76,36 +86,41 @@ player_performance_clean <- player_performance_metadata %>%
   left_join(player_performance_raw, by = c('name', 'birthyear', 'Year', 'age', 'nation')) %>%
   select(-name, -birthyear, -position)
 
-rm(player_performance_raw, player_performance_metadata)
-
 # clean clubs
 clubs_clean <- clubs %>% 
   select(club_id, name, domestic_competition_id,  national_team_players, net_transfer_record, stadium_seats) %>%
   rename(club_name = name)
 
-# count goals per player
-goals_per_season <- game_events %>% 
-  filter(type == 'Goals') %>%
-  left_join(games, by='game_id') %>%
-  mutate(year = year(date)) %>%
-  select(player_id, year) %>%
-  group_by(player_id, year) %>%
-  summarise(goals = n()) %>%
-  ungroup()
-
-## Join all tables together into one df
+## Join all tables together into one df and clear out irrelevant columns
 df_raw <- player_valuations_clean %>%
   left_join(players_clean, by='player_id') %>%
-  left_join(clubs_clean, by='club_id') %>%
-  inner_join(player_performance_clean, by=c('player_id', 'year' = 'Year'))
+  inner_join(player_performance_clean, by=c('player_id', 'year' = 'Year')) %>%
+  rename(season = year, player_name = name) %>%
+  select(-club_id, -current_club_name, -last_season, -birthyear, -goals_assists_per_90, -goals_assists_excl_pens_per_90,
+         -exp_goals_excl_pens, -exp_assisted_goals, -shots_per_90, -shots_on_target_per_90, -shots_from_freekicks,
+         -goals_minus_exp_goals, -goals_minus_exp_goals_excl_pen, -total_pass_distance, -short_passes_completed,
+         -short_passes_attempted, -short_passes_completed_pct, -medium_passes_attempted,
+         -medium_passes_completed_pct, -long_passes_attempted, -long_passes_completed, -long_passes_completed_pct,
+         -assists_minus_exp_assisted_goals, -completed_passes_att_third, -live_passes, -dead_passes, -passes_attempted_from_freekicks,
+         -passes_over_40yd_wide, -passes_in_offside, -passes_completed_throughballs, -inswinging_corners, -outswinging_corners,
+         -straight_corners, -passes_in_offside, -passes_blocked, -sca_pass_live, -sca_pass_dead, -sca_defense, -sca_dribbling,
+         -sca_fouls_drawn, -sca_shot, -penalties_condeded) %>%
+  mutate(league = recode(league, "Germany" = "Bundesliga",
+                         "England" = "Premier League",
+                         "Italy" = "Serie A",
+                         "France" = "Ligue 1",
+                         "Spain" = "La Liga"))
 
-## Restrict Scope of data set
+## Restrict Scope of data set and filter out NA values
 df <- df_raw %>%
-  filter(year >= 2018) %>%
+  filter(season >= 2018) %>%
   filter(minutes_played >= 90) %>%
-  filter(position != 'Goalkeeper')
+  filter(position != 'Goalkeeper') %>%
+  filter(rowSums(is.na(select(., -c("agent_name", "contract_expiration_date", "shots_on_target_pct", 
+                                    "goals_per_shot", "goals_per_shot_on_target", "avg_goal_distance")))) == 0)
+  
 
-## Check which players from performance data are not correctly linked with transfermarkt data
+## Check which players from performance data are not correctly linked with transfer market data
 player_performance_not_in_df <- anti_join(player_performance_clean, player_valuations_clean,  by=c('player_id', 'Year' = 'year')) %>%
   group_by(player_id) %>%
   summarise() %>%
--- exploration_1.R
+++ exploration_1.R
@@ -6,7 +6,7 @@ library(psych)
 library(stargazer)
 
 # load data set
-df_raw <- read_csv("df.csv") %>% select(-...1)
+df_raw <- read_csv("df.csv") %>% select(-...1) %>% arrange(season)
 
 # split data set into test and training data set
 set.seed(123)
@@ -22,6 +22,9 @@ summ <- df %>%
 
 stargazer(as.data.frame(summ), median = T, title = 'Summary Statistics', omit.stat = 'N')
 
+# observations per year per league
+table(df$season, df$league)
+
 
 # first plots
 theme_set(theme_light())
@@ -32,12 +35,12 @@ ggplot(df, aes(x=goals, y=market_value_in_eur)) +
   geom_smooth(method='lm') +
   ggtitle('Market Value vs. Goals Scored in previous season by position')
 
-ggplot(df, aes(x=factor(year), y=market_value_in_eur)) +
+ggplot(df, aes(x=factor(season), y=market_value_in_eur)) +
   geom_boxplot() + 
   scale_y_log10() +
   ggtitle('Market Value Distribution by year')
 
-ggplot(df, aes(x=factor(domestic_competition_id), y=market_value_in_eur)) +
+ggplot(df, aes(x=factor(league), y=market_value_in_eur)) +
   geom_boxplot() + 
   scale_y_log10() +
   ggtitle('Market Value Distribution by league')
@@ -49,9 +52,23 @@ ggplot(df, aes(x=factor(position), y=market_value_in_eur)) +
 
 ggplot(df, aes(x=age, y=market_value_in_eur)) +
   geom_point() +
-  geom_smooth(method='lm') +
+  geom_smooth() +
   facet_wrap(~position) + 
   scale_y_log10()
   
-  
+df_raw %>%
+  group_by(player_id) %>%
+  mutate(mkt_value_lag = lag(market_value_in_eur)) %>%
+  ggplot(aes(x=mkt_value_lag, y=market_value_in_eur)) + geom_point(shape=1) + geom_smooth(method = 'lm') + scale_x_log10() + scale_y_log10() + geom_abline()
+
+df %>%
+  group_by(player_id) %>%
+  arrange()
+  mutate(mkt_value_lag = lag(market_value_in_eur)) %>%
+  filter(mkt_value_lag > 110000000, market_value_in_eur < 14000000)
+
+df %>%
+  filter(player_name == "Eden Hazard") %>%
+  arrange(season) %>%
+  select(market_value_in_eur, date_valuation, season)
 
